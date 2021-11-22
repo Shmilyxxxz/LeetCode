65911fa0c771d58270198cfcb2ddd4a17a5420ee
哲 徐 <151893140@qq.com>
2021-11-22 09:47:21 +0800

    add new practice

---
+++ LeetCode0397/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LeetCode0397
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class Solution
+        {
+            public int IntegerReplacement(int n)
+            {
+                int step = 0;
+                if (1 == n)
+                    return 0;
+                if (0==n%2)
+                {
+                    step++;
+                    return step + IntegerReplacement(n / 2);
+                }
+                else
+                {
+                    step += 2;
+                    if (1==n%4)
+                    {
+                        return step + IntegerReplacement(n / 2);
+                    }
+                    else 
+                    {
+                        if (3 == n)
+                        {
+                            return step;
+                        }
+                        else
+                        {
+
+                            return step + IntegerReplacement(n / 2 + 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
