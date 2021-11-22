bc5fa972889d747b7a944a0f4de14cf4b412d0dc
哲 徐 <151893140@qq.com>
2021-11-22 11:08:34 +0800

    add 0384

---
+++ LeetCode0384/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LeetCode0384
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public class Solution
+    {
+        public int[] originalArray;
+        public int[] nums;
+        public Solution(int[] nums)
+        {
+            this.nums = nums;
+            originalArray = new int[nums.Length];
+            this.nums.CopyTo(originalArray, 0);
+        }
+
+        public int[] Reset()
+        {
+            originalArray.CopyTo(nums, 0);
+            return nums;
+        }
+
+        public int[] Shuffle()
+        {
+            Random random = new Random();
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                int j = random.Next(i, nums.Length);
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            return nums;
+        }
+    }
+
+}
