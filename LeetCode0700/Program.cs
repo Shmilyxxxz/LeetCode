7076939cdf5345f378d84a10fddfed908318c13d
哲 徐 <151893140@qq.com>
2021-11-26 11:20:01 +0800

    add 700

---
+++ LeetCode0700/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LeetCode0700
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    //二叉搜索树满足如下性质：
+    //左子树所有节点的元素值均小于根的元素值；
+    //右子树所有节点的元素值均大于根的元素值。
+    public class Solution
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            if (val == root.val)
+            {
+                return root;
+            }
+            return SearchBST(val < root.val ? root.left : root.right, val);
+        }
+    }
+
+}
